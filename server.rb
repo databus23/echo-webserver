d554ea2e7f893403b1f6626d150ec189f0892eca
Fabian Ruff <fabian.ruff@sap.com>
2017-03-27 22:52:10 +0200

    Add Local hostname & ip & remote ip

--- server.rb
+++ server.rb
@@ -1,8 +1,13 @@
 #!/usr/local/bin/ruby
 require "webrick"
+require "socket"
+
 server = WEBrick::HTTPServer.new(:Port => ENV.fetch("PORT", 80))
 server.mount_proc("/") do |req,res| 
   res.body << req.request_line.chomp + "\n"
+  res.body << "Server Hostname: #{Socket.gethostname}\n"
+  res.body << "Local IP: #{req.addr[3]}\n"
+  res.body << "Remote IP: #{req.peeraddr[3]}\n"
   res.body << "Request headers:\n"
   res.body << req.header.collect{|k,v| ("%-20s -> %s" % [k, v.join(",")])}.join("\n")
   res.body << "\n"
